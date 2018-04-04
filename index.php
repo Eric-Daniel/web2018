84095d39db79c77fa35312e6c043b99f78c3730c
Eric Daniel <Eric-Daniel@users.noreply.github.com>
2018-04-04 18:21:56 +0800

    Improved Search n Filter UI!

--- index.php
+++ index.php
@@ -4,94 +4,77 @@
   require_once "./functions/database_functions.php";
 ?>
 
-<button onclick="myFunction()">Reload page/Back</button>
-
-<script>
-    function myFunction() {
-        location.reload();
-    }
-</script>
-
-<div class="page-header">
-    <h3>
-        <select id="year">
-            <option value="" selected="selected">Select Year</option>
-            <?php
-
-            $conn = new db_class();
-
-            $filter = $conn->filter();
-
-            while($fetch = $filter->fetch_array()){
-                ?>
-                <option value=<?php echo $fetch['year']; ?>><?php echo $fetch['year']; ?></option>
-            <?php }	?>
-            }
-        </select>
-    </h3>
-</div>
-
-<div class="page-header">
-    <h3>
-        <select id="genre">
-            <option value="" selected="selected">Select Genre</option>
-            <?php
-            $conn = new db_class();
-            $filter = $conn->filterGenre();
-            while($fetch = $filter->fetch_array()){
-               ?>
-                <option value=<?php echo $fetch['genre']; ?>><?php echo $fetch['genre']; ?></option>
-            <?php }	?>
-            }
-        </select>
-    </h3>
-</div>
-
-
-
-<body>
 <div class="container">
-    <br>
-    <h2 align="center">Test Ajax LS</h2>
-    <br>
-    <div class="form-group">
-        <div class="input-group"> <span class="input-group-addon">Search</span>
-            <input type="text" name="search_text"
-                   id="search_text" placeholder="Search By Title, Year or Genre" class="col-6">
-            <select id="year">
-                <option value="" selected="selected">Select Year</option>
-                <?php
-                $conn = new db_class();
-                $filter = $conn->filter();
-                while($fetch = $filter->fetch_array()){
-               ?>
-                    <option value=<?php echo $fetch['year']; ?>><?php echo $fetch['year']; ?></option>
-                <?php }	?>
-                }
-            </select>
-            <select id="genre">
-                <option value="" selected="selected">Select Genre</option>
-                <?php
+    <h5><button onclick="myFunction()">Reload page/Back</button></h5>
 
-                $conn = new db_class();
+    <script>
+        function myFunction() {
+            location.reload();
+        }
+    </script>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="dropdown">
+                <h5>
+                    <select id="year">
+                        <option value="" selected="selected">Select Year</option>
+                        <?php
 
-                $filter = $conn->filterGenre();
+                        $conn = new db_class();
 
-                while($fetch = $filter->fetch_array()){
-                ?>
-                    <option value=<?php echo $fetch['genre']; ?>><?php echo $fetch['genre']; ?></option>
-                <?php }	?>
-                }
-            </select>
+                        $filter = $conn->filter();
+                        //           ($stmt = $conn->query("$query"));
+                        while($fetch = $filter->fetch_array()){
+                            ?>
+                            <option value=<?php echo $fetch['year']; ?>><?php echo $fetch['year']; ?></option>
+                        <?php }	?>
+                        }
+                    </select>
+                    <select id="genre">
+                        <option value="" selected="selected">Select Genre</option>
+                        <?php
+                        $conn = new db_class();
+
+                        $filter = $conn->filterGenre();
+
+                        while($fetch = $filter->fetch_array()){
+                            ?>
+                            <option value=<?php echo $fetch['genre']; ?>><?php echo $fetch['genre']; ?></option>
+                        <?php }	?>
+                        }
+                    </select>
+                </h5>
+            </div>
         </div>
     </div>
-    <br>
-    <div id="result" style="position:absolute;background-color:powderblue;"></div>
-</div>
-</body>
+    <body>
+    <div class="container">
+        <br>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">SEARCH</span>
+            </div>
+            <input type="text" name="search_text" id="search_text" class="form-control" placeholder="Search By Title, Year or Genre" aria-label="SEARCH" aria-describedby="basic-addon1">
+
+        </div>
+
+
+        <div class="form-group">
+            <!--        <div class="input-group input-group-lg">-->
+            <!--            <div class="input-group"> <span class="input-group-addon" id="sizing-addon1">SEARCH</span>-->
+            <!--             <input type="text" name="search_text" class="form-control" id="search_text" placeholder="Search By Title, Year or Genre" aria-describedby="sizing-addon1">-->
+            <!--            </div>-->
+            <!--        </div>-->
+            <!---->
+            <!--    <br>-->
+            <div id="result" style="position:absolute;background-color:powderblue;"></div>
+        </div>
+    </div>
+    </body>
 
 
 <table class="table" style="margin-top: 20px">
+    <br>
     <tr>
         <th>Title</th>
         <th>Year</th>
